596168fa653f90a98ee17b7b999fdcce215ec339
Martha Kelly <mk.girdler@gmail.com>
2012-08-01 16:02:54 -0400

    got the model form and submissions working, just need to redirect after form submit

--- thxyew/thxyew_note/models.py
+++ thxyew/thxyew_note/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.forms import ModelForm
 
 class Author(models.Model):
     name = models.CharField(max_length=64)
@@ -10,9 +11,14 @@ class Author(models.Model):
 class Note(models.Model):
     to_person = models.CharField(max_length=100)
     author = models.ForeignKey(Author)
-    pub_date = models.DateTimeField()
+    pub_date = models.DateField(auto_now=True)
     subject = models.CharField(max_length=200)
     note_body = models.TextField()
 
     def __str__(self):
         return self.subject
+        
+class NoteForm(ModelForm):
+    class Meta:
+        model = Note
+
--- thxyew/thxyew_note/views.py
+++ thxyew/thxyew_note/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render_to_response
-from thxyew_note.models import Note, Author
+from thxyew_note.models import Note, Author, NoteForm
+from django.template import RequestContext
 
 def index(request):
     all_notes = Note.objects.all().order_by('-pub_date')
     template_data = {'notes' : all_notes}
 
     return render_to_response('index.html', template_data)
+    
+def write_note(request):
+    if request.method == 'POST':
+        form = NoteForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = NoteForm()
+
+    variables = RequestContext(request, {
+        'form': form
+    })
+
+    return render_to_response('write-note.html', variables)
