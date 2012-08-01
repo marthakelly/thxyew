19bb6baf9cf944147112f4ab972533e04a06e59f
Martha Kelly <mk.girdler@gmail.com>
2012-08-01 17:00:07 -0400

    have form submission working and redirect

--- thxyew/thxyew/urls.py
+++ thxyew/thxyew/urls.py
@@ -14,6 +14,7 @@ urlpatterns = patterns('',
 
     url(r'^$', 'thxyew_note.views.index'),
     url(r'^write-note', 'thxyew_note.views.write_note'),
+    url(r'^submit-note', 'thxyew_note.views.submit_note'),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
--- thxyew/thxyew_note/views.py
+++ thxyew/thxyew_note/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render_to_response
 from thxyew_note.models import Note, Author, NoteForm
 from django.template import RequestContext
+from django.http import HttpResponseRedirect
 
 def index(request):
     all_notes = Note.objects.all().order_by('-pub_date')
@@ -9,15 +10,21 @@ def index(request):
     return render_to_response('index.html', template_data)
     
 def write_note(request):
-    if request.method == 'POST':
-        form = NoteForm(request.POST)
-        if form.is_valid():
-            form.save()
-    else:
-        form = NoteForm()
+    form = NoteForm()
 
     variables = RequestContext(request, {
         'form': form
     })
 
+    #return render_to_response('write-note.html', variables)
     return render_to_response('write-note.html', variables)
+
+def submit_note(request):
+    if request.method == 'POST':
+        form = NoteForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = NoteForm()    
+
+    return HttpResponseRedirect('/')
