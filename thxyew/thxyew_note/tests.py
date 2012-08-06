1061c2056d596cc24bc7f3566836445a0a832a70
Martha Kelly <mk.girdler@gmail.com>
2012-08-06 10:20:55 -0400

    testing deploy:

--- thxyew/thxyew_note/tests.py
+++ thxyew/thxyew_note/tests.py
@@ -1,16 +1,25 @@
 from django.test import TestCase
 
 class NoteTest(TestCase):
-    #fixtures = ['thxyew_note_testdata.json']
-    def text_index(self):
+
+    def test_index(self):
         resp = self.client.get('/')
         self.assertEqual(resp.status_code, 200)
+
+'''
         #testbook = Book.objects.all()[0]
         #url = '/books/id/' + str(testbook.id)
         #response = self.client.get(url)
         #self.assertContains(response, testbook.title)
     
-#index
-#write_note
-#submit_note
-#single_note
+index
+write_note
+submit_note
+single_note
+def setUp(self):
+    pass
+
+  def tearDown(self):
+    pass
+#fixtures = ['thxyew_note_testdata.json']
+'''
